5f45272854973b5102c3858693d97cb9dc9e41e8
joaquimcarmona <jcarmona@oninet.pt>
2016-03-07 11:23:02 +0000

    Update FileEchoer.java

--- src/main/java/FileEchoer.java
+++ src/main/java/FileEchoer.java
@@ -17,38 +17,39 @@ public class FileEchoer extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
       
+        	System.out.println("A");
         	InputStream ins = null; // raw byte-stream
-			Reader r = null; // cooked reader
-			BufferedReader br = null; // buffered for readLine()
-			try {
+		Reader r = null; // cooked reader
+		BufferedReader br = null; // buffered for readLine()
+		try {
     			String s;
-            if (request.getParameter("filename") == null) {
-        			System.out.println("No file name");
-            } else {
-        			String filename = request.getParameter("filename").toString();
-               ins = new ByteArrayInputStream(filename.getBytes());
-               r = new InputStreamReader(ins, "UTF-8"); // leave charset out for default
+            	if (request.getParameter("filename") == null) {
+        		System.out.println("No file name");
+        	 } else {
+        		String filename = request.getParameter("filename").toString();
+               		ins = new ByteArrayInputStream(filename.getBytes());
+               		r = new InputStreamReader(ins, "UTF-8"); // leave charset out for default
     				br = new BufferedReader(r);
     				while ((s = br.readLine()) != null) {
        				System.out.println(s);
     				}
     			}
-         }
-			catch (Exception e)
+        	}
+		catch (Exception e)
 			{
     			System.err.println(e.getMessage()); // handle exception
 			}
-			finally {
+		finally {
     			if (br != null) 
-            	{ try { br.close(); } 
-         		catch(Throwable t) { /* ensure close happens */ } }
+            			{ try { br.close(); } 
+         			catch(Throwable t) { /* ensure close happens */ } }
     			if (r != null) 
-            	{ try { r.close(); } 
-               catch(Throwable t) { /* ensure close happens */ } }
+            			{ try { r.close(); } 
+               			catch(Throwable t) { /* ensure close happens */ } }
     			if (ins != null) 
-            	{ try { ins.close(); } 
-               catch(Throwable t) { /* ensure close happens */ } }
-			}     
-    }
+            			{ try { ins.close(); } 
+               			catch(Throwable t) { /* ensure close happens */ } }
+		}     
+    	}
   
 }
