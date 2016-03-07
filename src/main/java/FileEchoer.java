db391c87e0863a7a2e3b622b66c7d564996551fb
joaquimcarmona <jcarmona@oninet.pt>
2016-03-07 14:02:58 +0000

    Update FileEchoer.java

--- src/main/java/FileEchoer.java
+++ src/main/java/FileEchoer.java
@@ -14,7 +14,7 @@ import java.io.InputStreamReader;
 public class FileEchoer extends HttpServlet {
   
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
       
         	System.out.println("A");
